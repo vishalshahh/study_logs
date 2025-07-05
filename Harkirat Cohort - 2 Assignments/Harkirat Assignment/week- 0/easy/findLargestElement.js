39735e8f3fb1cdac937639c2a674927d0f1c75d6
Vishal Shah <vishal12kl@gmail.com>
2025-07-05 14:09:21 +0530

    #3: find largest element

--- Harkirat Cohort - 2 Assignments/Harkirat Assignment/week- 0/easy/findLargestElement.js
+++ Harkirat Cohort - 2 Assignments/Harkirat Assignment/week- 0/easy/findLargestElement.js
@@ -6,7 +6,18 @@
 */
 
 function findLargestElement(numbers) {
-    
-}
-
-module.exports = findLargestElement;
+    // Step 1: Assume first number is the largest
+    let largest = numbers[0];
+  
+    // Step 2: Loop through the rest of the array
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > largest) {
+        largest = numbers[i]; // Update if current is larger
+      }
+    }
+  
+    return largest;
+  }
+  
+  module.exports = findLargestElement;
+  
